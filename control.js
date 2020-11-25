45bc5e1143853afea8a5c7a0c88f154cc4422727
shivanshpaliwal0005 <75037125+shivanshpaliwal0005@users.noreply.github.com>
2020-11-25 23:46:41 +0530

    Update control.js

--- control.js
+++ control.js
@@ -2,11 +2,7 @@ let data = {
   "enable": true
 };
 
-setTimeout(()=>{
-  alert("111111111111111100");
-  console.log("-----------------", data);
-}, 2000)
-
+console.log("111111111111111100");
 if(!data.enable){
   console.log("-----------------", "");
   document.querySelector('body').remove();
