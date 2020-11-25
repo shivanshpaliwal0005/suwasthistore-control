b7445dd845c65264b25de539839272cccb3bba8f
shivanshpaliwal0005 <75037125+shivanshpaliwal0005@users.noreply.github.com>
2020-11-26 00:11:14 +0530

    Update control.js

--- control.js
+++ control.js
@@ -3,9 +3,7 @@
     "enable": false
   };
 
-  console.log("111111111111111100", data.enable);
   if(!data.enable){
-    console.log("-----------------", "");
     document.querySelector('body').remove();
   }
 
